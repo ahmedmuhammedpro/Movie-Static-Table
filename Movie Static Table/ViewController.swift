c860c8a7a8100feb420a738d896ba42515df6efa
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-04-02 15:48:35 +0200

    Initial Commit

---
+++ Movie Static Table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Static Table
+//
+//  Created by ahmedpro on 4/2/20.
+//  Copyright © 2020 ahmedpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
